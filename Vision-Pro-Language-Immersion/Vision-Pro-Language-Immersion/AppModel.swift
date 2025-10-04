1ca68802989e30d31eee45096594651ba23c3496
urnice1111 <133076202+urnice1111@users.noreply.github.com>
2025-10-03 21:56:32 -0400

    Project started

---
+++ Vision-Pro-Language-Immersion/Vision-Pro-Language-Immersion/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Vision-Pro-Language-Immersion
+//
+//  Created by UwU on 03/10/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Vision-Pro-Language-Immersion/Vision-Pro-Language-Immersion/Vision_Pro_Language_ImmersionApp.swift
@@ -0,0 +1,33 @@
+//
+//  Vision_Pro_Language_ImmersionApp.swift
+//  Vision-Pro-Language-Immersion
+//
+//  Created by UwU on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Vision_Pro_Language_ImmersionApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
